f730d2f9d861c382b1d09642f0d93078ba71cdb6
praneeth4u152 <38998102+praneeth4u152@users.noreply.github.com>
2019-05-31 14:45:52 +0530

    Add files via upload

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,83 @@
+import { WelcomeDataService } from './../service/data/welcome-data.service';
+import { ActivatedRoute } from '@angular/router';
+//package com.in28minutes.springboot.web;
+
+//import org.springframework.boot.SpringApplication;
+import { Component, OnInit } from '@angular/core';//'./app.component';
+//import { AppComponent } from '../app.component';
+
+//@ComponentScan(
+//      value = "com.in28minutes.springboot.web")
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+
+//public class SpringBootFirstWebApplication implements SomeInterface{
+export class WelcomeComponent implements OnInit {
+
+  message = 'Some Welcome Message'
+  welcomeMessageFromService:string
+  name = ''
+  //String message = "Some Welcome Message"
+  
+  //public SpringBootFirstWebApplication() {	
+
+  //ActivatedRoute
+  constructor(
+    private route:ActivatedRoute,
+    private service:WelcomeDataService) { 
+
+  }
+
+  // void init() {
+  ngOnInit(){
+    //COMPILATION ERROR this.message = 5
+    //console.log(this.message)
+    // console.log(this.route.snapshot.params['name'])
+    this.name = this.route.snapshot.params['name'];
+    
+  }
+
+  getWelcomeMessage() {
+   
+    
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+    
+   
+  }
+
+  getWelcomeMessageWithParameter() {
+    //console.log(this.service.executeHelloWorldBeanService());
+    
+    this.service.executeHelloWorldServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+    
+    
+  }
+
+
+  handleSuccessfulResponse(response){
+    this.welcomeMessageFromService = response.message
+   
+  }
+
+  handleErrorResponse(error) {
+    
+    this.welcomeMessageFromService = error.error.message
+  }
+}
+
+export class Class1 {
+
+}
+
+export class Class2 {
+  
+}
